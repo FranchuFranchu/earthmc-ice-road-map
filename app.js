9d66151221caa8ebcec9fb095a9a2c2722fb7bd6
imabritishcow <m@bcow.tk>
2020-07-25 01:37:37 -0400

    Dark mode

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 mdc.autoInit();
 
-am4core.ready(function() {
-  loadChart()
-  
-  loadData()
-  Object.keys(connections).forEach(function(k, i) {
-      
-      for (let i = 0; i < connections[k].length; i++) {
-          p0 = citiesL[connections[k][i][0]]
-          p1 = citiesL[connections[k][i][1]]
-          if (p1 == undefined || p0 == undefined) {
-              console.log("Can't make " + connections[k][i][0] + " to " + connections[k][i][1])
-              continue
-          }
-          addLine(p0, p1, k)
-      }
-  
-  });
-  
-  
-  console.log($("#toggleDarkMode"))
-  
-  if (localStorage.darkMode == "true") {
-      $("#toggleDarkMode")[0].checked = true
+function darkModeCheck() {
+  if (localStorage.darkMode === "true") {
+    dark();
   } else {
-      $("#toggleDarkMode")[0].checked = false
-  
+    light();
   }
-  
-  toggleDarkMode($("#toggleDarkMode")[0])
-  
-  })
+}
+
+am4core.ready(function () {
+  loadChart()
+
+  darkModeCheck()
+
+  loadData()
+  Object.keys(connections).forEach(function (k, i) {
+
+    for (let i = 0; i < connections[k].length; i++) {
+      p0 = citiesL[connections[k][i][0]]
+      p1 = citiesL[connections[k][i][1]]
+      if (p1 == undefined || p0 == undefined) {
+        console.log("Can't make " + connections[k][i][0] + " to " + connections[k][i][1])
+        continue
+      }
+      addLine(p0, p1, k)
+    }
+
+  });
+})
 
 let panelOpen = false;
 let panel = document.getElementById('panel');
@@ -45,14 +42,14 @@ function panelToggle() {
       x: -600,
       duration: 0.35
     });
-    setTimeout(function() {
+    setTimeout(function () {
       panel.style.display = "none";
     }, 350)
   } else {
     panelOpen = true;
     panel.style.display = "block";
     gsap.to(".panel", {
-      x: 0, 
+      x: 0,
       duration: 0.5
     });
   }
