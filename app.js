0c70de5311736b428d0573759f5fcf230e9ccd7c
imabritishcow <m@bcow.tk>
2020-07-24 22:12:16 -0400

    Moved <script> into app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+am4core.ready(function() {
+  loadChart()
+  
+  loadData()
+  Object.keys(connections).forEach(function(k, i) {
+      
+      for (let i = 0; i < connections[k].length; i++) {
+          p0 = citiesL[connections[k][i][0]]
+          p1 = citiesL[connections[k][i][1]]
+          if (p1 == undefined || p0 == undefined) {
+              console.log("Can't make " + connections[k][i][0] + " to " + connections[k][i][1])
+              continue
+          }
+          addLine(p0, p1, k)
+      }
+  
+  });
+  
+  
+  console.log($("#toggleDarkMode"))
+  
+  if (localStorage.darkMode == "true") {
+      $("#toggleDarkMode")[0].checked = true
+  } else {
+      $("#toggleDarkMode")[0].checked = false
+  
+  }
+  
+  toggleDarkMode($("#toggleDarkMode")[0])
+  
+  })
